87b2353ac84b54e519c8d9d496c40d2ebed26181
Will <wbaik7022@stu.d214.org>
2025-12-17 09:53:42 -0600

    Initial Commit

---
+++ ActivityFinder/ActivityFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityFinderApp.swift
+//  ActivityFinder
+//
+//  Created by William J. Baik on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ActivityFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActivityFinder
+//
+//  Created by William J. Baik on 12/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
