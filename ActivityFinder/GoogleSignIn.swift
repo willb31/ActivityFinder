884921a7d686235e260e1e955159d5eb471fb945
Alexander Kuklisnki <akuklinski7210@stu.d214.org>
2026-01-14 11:18:15 -0600

    I Began on the viewmodel specifically working on the functions that will work with the google sign in and the database seamlessly

---
+++ ActivityFinder/GoogleSignIn.swift
@@ -0,0 +1,25 @@
+//
+//  GoogleSignIn.swift
+//  ActivityFinder
+//
+//  Created by Alexander Kuklinski on 1/14/26.
+//
+
+import SwiftUI
+
+
+struct GoogleSignInView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    GoogleSignInView()
+}
