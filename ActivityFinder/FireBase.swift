99d84228cdc9309bb22dbdd98374e46ff7712c72
Alexander Kuklisnki <akuklinski7210@stu.d214.org>
2026-01-16 10:50:28 -0600

    worked on 2 viewmodel functions to add the functionalities of signing in with google and creating a new user document into the database

--- ActivityFinder/FireBase.swift
+++ ActivityFinder/FireBase.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 import Firebase
 import GoogleSignIn
+import FirebaseAuth
 
 @main
 struct ActivityFinder: App {
     
     init(){
         FirebaseApp.configure()
+        
+        Auth.auth().signInAnonymously { result, error in
+            if let error = error {
+                print("error")
+            }
+            else {
+                print("Signed in")
+            }
+        }
     }
     var body: some Scene {
         WindowGroup {
