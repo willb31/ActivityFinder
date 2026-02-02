07afe0a10dfdc9d7d1ede4412adee76f0398ff7b
Alexander Kuklisnki <akuklinski7210@stu.d214.org>
2026-02-02 11:17:34 -0600

    added top tab view

--- ActivityFinder/ContentView.swift
+++ ActivityFinder/ContentView.swift
@@ -11,25 +11,24 @@ import Firebase
 struct ContentView: View {
     @State var authManager = AuthenticationManager()
     let columns = [
-            GridItem(.flexible()),
-            GridItem(.flexible())
-        ]
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
+        TabView()
         VStack {
-            Spacer()
-            
-           Text("Hello, \(authManager.user?.displayName ?? "User")!")
-            
+            Text("Hello, \(authManager.user?.displayName ?? "User")!")
+                .font(.title)
             ScrollView {
-                            LazyVGrid(columns: columns, spacing: 16) {
-                                ForEach(authManager.clubs) { club in
-                                    ClubCardView(club: club)
-                                }
-                            }
-                            .padding()
-                        }
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(authManager.clubs) { club in
+                        ClubCardView(club: club)
+                    }
+                }
+                .padding()
+            }
             
-            Button("Clear Sign-In") {
+            Button("Clear Sign-In (Temporary)") {
                 authManager.signOut()
                 
             }
@@ -41,11 +40,11 @@ struct ContentView: View {
 }
 struct ClubCardView: View {
     let club: Club
-
+    
     var body: some View {
         VStack {
             Text(club.name)
-               
+            
         }
         .padding()
         .frame(maxWidth: .infinity)
@@ -53,3 +52,30 @@ struct ClubCardView: View {
         .cornerRadius(10)
     }
 }
+struct TabView: View {
+    @State var authManager = AuthenticationManager()
+    var body: some View {
+        
+            
+            HStack {
+                Image("HerseyLogo")
+                    .resizable()
+                    .frame(width: 130, height: 80, alignment: .topLeading)
+                    .padding()
+    
+                
+            Spacer()
+                Button{}label: {
+                    Image(systemName: "line.3.horizontal")
+                        .padding()
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                }
+            }
+            
+            .background(Color.orange)
+            
+        
+    }
+}
+
