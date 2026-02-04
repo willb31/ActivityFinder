098e3228be83ffcf2b88a51cef54ddf52cfa7d25
Alexander Kuklisnki <akuklinski7210@stu.d214.org>
2026-02-04 10:44:25 -0600

    small fixes

--- ActivityFinder/ContentView.swift
+++ ActivityFinder/ContentView.swift
@@ -16,33 +16,6 @@ struct ContentView: View {
     ]
     var body: some View {
         TabView()
-        VStack {
-            Text("Hello, \(authManager.user?.displayName ?? "User")!")
-                .font(.title)
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 16) {
-                    ForEach(authManager.clubs) { club in
-                        ClubCardView(club: club)
-                    }
-                }
-                .padding()
-            }
-            
-            Button("Clear Sign-In (Temporary)") {
-                authManager.signOut()
-                
-            }
-        }
-    }
-}
-#Preview {
-    ContentView()
-}
-
-struct TabView: View {
-    @State var authManager = AuthenticationManager()
-    var body: some View {
-        TabView()
         NavigationStack{
             VStack {
                 Text("Hello, \(authManager.user?.displayName ?? "User")!")
@@ -55,22 +28,24 @@ struct TabView: View {
                     }
                     .padding()
                 }
-                
-                HStack{
-                    Spacer()
-                    Button("Clear Sign-In (Temporary)") {
-                        authManager.signOut()
-                        
-                    }
-                    Spacer()
-                    NavigationLink("add club"){
-                        AddClubView()
-                    }
-                    Spacer()
+            }
+            HStack{
+                Spacer()
+                Button("Clear Sign-In (Temporary)") {
+                    authManager.signOut()
+                    
                 }
+                Spacer()
+                NavigationLink("Add club (temporary)"){
+                    AddClubView()
+                }
+                Spacer()
             }
         }
     }
+}
+#Preview {
+    ContentView()
 }
 #Preview {
     ContentView()
