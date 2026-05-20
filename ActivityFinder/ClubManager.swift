6ecbbdcc9eeffc70464ce1d1457acf3584e67229
Will <wbaik7022@stu.d214.org>
2026-05-20 11:01:39 -0500

    Add Codable to Club and implement FavoritesManager with UserDefaults persistence

--- ActivityFinder/ClubManager.swift
+++ ActivityFinder/ClubManager.swift
@@ -16,3 +16,27 @@ import FirebaseFirestore
     
     let database = Firestore.firestore()
 }
+
+@Observable class FavoritesManager {
+    var savedClubs: [Club] = []
+    private let savedKey = "favoriteClubs"
+    init() { load() }
+    func isFavorited(_ club: Club) -> Bool {
+        savedClubs.contains(where: { $0.id == club.id })
+    }
+    func toggle(_ club: Club) {
+        if isFavorited(club) { savedClubs.removeAll { $0.id == club.id } }
+        else { savedClubs.append(club) }
+        save()
+    }
+    private func save() {
+        if let data = try? JSONEncoder().encode(savedClubs) {
+            UserDefaults.standard.set(data, forKey: savedKey)
+        }
+    }
+    private func load() {
+        guard let data = UserDefaults.standard.data(forKey: savedKey),
+              let clubs = try? JSONDecoder().decode([Club].self, from: data) else { return }
+        savedClubs = clubs
+    }
+}
